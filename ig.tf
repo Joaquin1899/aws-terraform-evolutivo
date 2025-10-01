9427545f0f419a6edeb26a07269e7cd00a0af89a
Joaquin1899 <cloud@192.168.1.8>
2025-10-01 17:12:58 -0300

    Crear IG en etapa 3

---
+++ ig.tf
@@ -0,0 +1,26 @@
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "terra-ig"
+  }
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "rutas-principal"
+  }
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
